8a5de79bfb60dafc5d7e1854deb75b81ba21445e
Nilton <niltonlqjr@gmail.com>
2026-04-23 00:45:08 -0300

    4 operacoes basicas

--- float/ex2.s
+++ float/ex2.s
@@ -1,22 +1,36 @@
 .global _start
 
 .section .data
-    value1: .tfloat 3.14159 
-    value2: .tfloat 2.0
-    value_ini: .tfloat 0.0
+    valor1: .double 3.14159 
+    valor2: .double 2.0
+
 
 .section .bss
-    .lcomm resposta, 8
+    .lcomm soma, 8
+    .lcomm subt, 8
+    .lcomm mult, 8
+    .lcomm divi, 8
 
 .section .text
 _start:
-    fldt value1
-    fldt value2
-    fldt value_ini
-    fadd %st(1), %st
-    fmul %st(2)
-    fst resposta
     
+    fldl valor2
+    fldl valor1
+    fadd %st(1), %st
+    fstpl soma
+
+    fldl valor1
+    fsubl valor2 # %st(0) = %st(0) - valor2
+    fstpl subt
+    
+    fldl valor1
+    fmul %st(1), %st(0)
+    fstpl mult
+
+    fldl valor1
+    fdiv %st(0), %st(1)
+    fstpl divi
+
     mov $60, %rax
     mov $0, %rdi
     syscall
